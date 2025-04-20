f8e3269cb6607a54bf32ff500561e5221f42da5f
lisaliu <lisaliu9194@gmail.com>
2025-04-20 16:57:14 -0700

    Delete extra folder

--- cs5610-final-project/src/Kambaz/Account/FacultyFeatures.tsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux";
-
-export default function FacultyFeatures({ children }: { children: any }) {
-  const { currentUser } = useSelector((state: any) => state.accountReducer);
-  if (currentUser && currentUser.role == "FACULTY") {
-    return children;
-  } else {
-    return;
-}}
--- cs5610-final-project/src/Kambaz/Database/index.ts
+++
@@ -1,9 +0,0 @@
-import courses from "./courses.json";
-import modules from "./modules.json";
-import assignments from "./assignments.json";
-import users from "./users.json";
-import enrollments from "./enrollments.json";
-import quizzes from "./quizzess.json";
-import questions from "./questions.json";
-
-export {  courses, modules, assignments, users, enrollments, quizzes, questions  };
